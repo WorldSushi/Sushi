52afe2a10bdd0ac4cb821edf5898c9e1f871da72
LettoLt <Letto-l@yandex.ru>
2019-04-18 13:21:31 +0300

    1

---
+++ Data/Services/Abstract/ClientContacts/IMonthlyCallPlanService.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Data.Commands.ClientContacts;
+using Data.Entities.ClientContacts;
+
+namespace Data.Services.Abstract.ClientContacts
+{
+    public interface IMonthlyCallPlanService
+    {
+        IQueryable<MonthlyCallPlan> GetAll();
+
+        void Create(MonthlyCallPlanCreateCommand command);
+    }
+}
---
+++ Data/Services/Concrete/ClientContacts/MonthlyCallPlanService.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Base;
+using Data.Commands.ClientContacts;
+using Data.Entities.ClientContacts;
+using Data.Services.Abstract.ClientContacts;
+
+namespace Data.Services.Concrete.ClientContacts
+{
+    public class MonthlyCallPlanService : IMonthlyCallPlanService
+    {
+        private readonly IRepository<MonthlyCallPlan> _monthlyCallPlanRepository;
+
+        public MonthlyCallPlanService(IRepository<MonthlyCallPlan> monthlyCallPlanRepository)
+        {
+            _monthlyCallPlanRepository = monthlyCallPlanRepository;
+        }
+
+        public IQueryable<MonthlyCallPlan> GetAll()
+        {
+            return _monthlyCallPlanRepository.All();
+        }
+
+        public void Create(MonthlyCallPlanCreateCommand command)
+        {
+            _monthlyCallPlanRepository.Create(
+                new MonthlyCallPlan(
+                    command.ManagerId,
+                    command.ClientId,
+                    command.AmountCalls,
+                    command.Month));
+        }
+    }
+}
