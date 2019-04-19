6aa99d054c39ebf529f8b5961242a050a3e04826
Shushuka20 <ilyaklepov@gmail.com>
2019-04-19 12:10:53 +0300

    подкапотная крад логика

--- WebUI/wwwroot/src/app/admin/store/actions/user.action.ts
+++ WebUI/wwwroot/src/app/admin/store/actions/user.action.ts
@@ -5,8 +5,18 @@ import { Manager } from '../../models/manager.model';
 export enum ManagerActionTypes {
     GET_ALL = 'Get All Managers',
     GET_ALL_SUCCESS = 'Get All Managers Success',
-    GET_ALL_FAILURE = 'Get All Managers Failure'
+    GET_ALL_FAILURE = 'Get All Managers Failure',
+    CREATE_MANAGER = 'Create Manager',
+    CREATE_MANAGER_SUCCESS = 'Create Manager Success',
+    CREATE_MANAGER_FAILURE = 'Create Manager Failure',
+    UPDATE_MANAGER = 'Update Manager',
+    UPDATE_MANAGER_SUCCESS = 'Update Manager Success',
+    UPDATE_MANAGER_FAILURE = 'Update Manager Failure',
+    DELETE_MANAGER = 'Delete Manager',
+    DELETE_MANAGER_SUCCESS = 'Delete Manager Success',
+    DELETE_MANAGER_FAILURE = 'Delete Manager Failure'
 }
+
 
 export class GetAll implements Action {
     readonly type = ManagerActionTypes.GET_ALL;
@@ -26,4 +36,69 @@ export class GetAllFailure implements Action {
     constructor(public payload: {error: string}) {}
 }
 
-export type ManagerActions = GetAll | GetAllSuccess | GetAllFailure
+export class CreateManager implements Action {
+    readonly type = ManagerActionTypes.CREATE_MANAGER;
+
+    constructor(public payload: {data: Manager}){}
+}
+
+export class CreateManagerSuccess implements Action {
+    readonly type = ManagerActionTypes.CREATE_MANAGER_SUCCESS;
+
+    constructor(public payload: {data: Manager}){}
+}
+
+export class CreateManagerFailure implements Action {
+    readonly type = ManagerActionTypes.CREATE_MANAGER_FAILURE;
+
+    constructor(public payload: {error: any}){}
+}
+
+export class UpdateManager implements Action {
+    readonly type = ManagerActionTypes.UPDATE_MANAGER;
+
+    constructor(public payload: {data: Manager}){}
+}
+
+export class UpdateManagerSuccess implements Action {
+    readonly type = ManagerActionTypes.UPDATE_MANAGER_SUCCESS;
+
+    constructor(public payload: {data: Manager}){}
+}
+
+export class UpdateManagerFailure implements Action {
+    readonly type = ManagerActionTypes.UPDATE_MANAGER_FAILURE;
+
+    constructor(public payload: {error: any}){}
+}
+
+export class DeleteManager implements Action {
+    readonly type = ManagerActionTypes.DELETE_MANAGER;
+
+    constructor(public payload: {data: number}){}
+}
+
+export class DeleteManagerSuccess implements Action {
+    readonly type = ManagerActionTypes.DELETE_MANAGER_SUCCESS;
+
+    constructor(public payload: {data: number}) { }
+}
+
+export class DeleteManagerFailure implements Action {
+    readonly type = ManagerActionTypes.DELETE_MANAGER_FAILURE;
+
+    constructor(public payload: {error: any}) { }
+}
+
+export type ManagerActions = GetAll 
+    | GetAllSuccess 
+    | GetAllFailure
+    | CreateManager
+    | CreateManagerSuccess
+    | CreateManagerFailure
+    | UpdateManager
+    | UpdateManagerSuccess
+    | UpdateManagerFailure
+    | DeleteManager  
+    | DeleteManagerSuccess
+    | DeleteManagerFailure
--- WebUI/wwwroot/src/app/admin/store/effects/manager.effects.ts
+++ WebUI/wwwroot/src/app/admin/store/effects/manager.effects.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import { Effect, ofType, Actions } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, catchError } from 'rxjs/operators';
 import { ManagerService } from '../../services/manager.service';
-import { GetAll, ManagerActionTypes, GetAllSuccess } from "../actions/user.action";
+import { GetAll, ManagerActionTypes, GetAllSuccess, CreateManager, CreateManagerSuccess, CreateManagerFailure, UpdateManager, UpdateManagerSuccess, UpdateManagerFailure, DeleteManager, DeleteManagerSuccess, DeleteManagerFailure } from "../actions/user.action";
 import { Manager } from "../../models/manager.model";
 
 @Injectable()
@@ -12,8 +12,33 @@ export class ManagerEffects {
     getManagers$ = this.actions$.pipe(
         ofType<GetAll>(ManagerActionTypes.GET_ALL),
         switchMap(() => this.managerService.getManagers()),
-        switchMap((managers: Manager[]) => of(new GetAllSuccess({managers: managers})) )
+        switchMap((managers: Manager[]) => of(new GetAllSuccess({managers: managers})))
     )
+
+    @Effect()
+    createManager$ = this.actions$.pipe(
+        ofType<CreateManager>(ManagerActionTypes.CREATE_MANAGER),
+        switchMap((action) => this.managerService.createManager(action.payload.data)),
+        switchMap((manager: Manager) => of(new CreateManagerSuccess({data: manager}))),
+        catchError((error: any) => of(new CreateManagerFailure({error: error})))
+    )
+
+    @Effect()
+    updateManager$ = this.actions$.pipe(
+        ofType<UpdateManager>(ManagerActionTypes.UPDATE_MANAGER),
+        switchMap((action) => this.managerService.updateManager(action.payload.data)),
+        switchMap((manager: Manager) => of(new UpdateManagerSuccess({data: manager}))),
+        catchError((error: any) => of(new UpdateManagerFailure({error: error})))
+    )
+
+    @Effect()
+    deleteManager$ = this.actions$.pipe(
+        ofType<DeleteManager>(ManagerActionTypes.DELETE_MANAGER),
+        switchMap((action) => this.managerService.deleteManager(action.payload.data)),
+        switchMap((id: number) => of(new DeleteManagerSuccess({data: id}))),
+        catchError((error: any) => of(new DeleteManagerFailure({error: error})))
+    )
+
 
     constructor(
         private managerService: ManagerService,
