f911b45053316d4a04f0e9efc5919f45f2398ed5
Denis <letto-l@yandex.ru>
2019-05-18 19:22:00 +0300

    weekplan

---
+++ Data/Entities/ClientContacts/WeekPlan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Base;
+using Data.Entities.Clients;
+using Data.Entities.Users;
+
+namespace Data.Entities.ClientContacts
+{
+    public class WeekPlan : Entity
+    {
+        public int ManagerId { get; set; }
+        public Manager Manager { get; set; }
+
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int WeekNumber { get; set; }
+
+        public string Plan { get; set; }
+
+        public string Fact { get; set; }
+
+        private WeekPlan()
+        {
+
+        }
+
+        public WeekPlan(int managerId, int clientId, int month, int weekNumber, string plan)
+        {
+            ManagerId = managerId;
+            ClientId = clientId;
+            Plan = plan;
+            WeekNumber = weekNumber;
+            Date = new DateTime(DateTime.Now.Year, month, 1);
+        }
+    }
+}
