7b41fa0216eecfb8015025625ed55548fc3b4a23
Ilya <ilyaklepov@gmail.com>
2019-06-16 14:21:58 +0300

    редакс для апп  и правка названия

---
+++ WebUI/ClientApp/src/app/store/app/reducers/index.ts
@@ -0,0 +1,6 @@
+import { ActionReducerMap } from '@ngrx/store';
+import { IAppState } from '../states';
+
+
+export const appReducer: ActionReducerMap<IAppState, any> = {
+}
---
+++ WebUI/ClientApp/src/app/store/root-store.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { appReducer } from './app/reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ManagerRmModule } from '../manager-rm/manager-rm.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,  
+    StoreModule.forRoot(appReducer),  
+    EffectsModule.forRoot([]),
+    ManagerRmModule,
+    StoreDevtoolsModule.instrument({}),
+  ]
+})
+export class RootStoreModule { }
