d4c4fa7c002f07b5dcb9c5de8312a31d642afcb5
Denis <letto-l@yandex.ru>
2019-07-01 15:48:20 +0300

    API ClientContactResult заглушка

---
+++ WebUI/ApiControllers/Manager/ClientContactsResultController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Data;
+using Data.DTO.Clients;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ApiControllers.Manager
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientContactsResultController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public ClientContactsResultController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = new List<ResultDto>()
+            {
+                new ResultDto()
+                {
+                    ClientId = 1,
+                    EscortCalls = 30,
+                    EscortMails = 2,
+                    EscortWhatsUpMessages = 1,
+                    RegionalCalls = 20,
+                    RegionalMails = 1,
+                    RegionalWhatsUpMessages = 0
+                }
+            };
+
+            return Ok(result);
+        }
+    }
+}
