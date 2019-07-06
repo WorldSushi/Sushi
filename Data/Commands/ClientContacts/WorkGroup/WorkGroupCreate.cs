2abf11a5c370f90380b6e061fe136082c957cb9f
Denis <letto-l@yandex.ru>
2019-07-06 13:48:32 +0300

    WorkGroup

---
+++ Data/Commands/ClientContacts/WorkGroup/WorkGroupCreate.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.Commands.ClientContacts.WorkGroup
+{
+    public class WorkGroupCreate
+    {
+        public int ClientId { get; set; }
+        public int EscortManagerId { get; set; }
+        public int RegionalManagerId { get; set; }
+    }
+}
---
+++ Data/Entities/ClientContacts/WorkGroup.cs
@@ -0,0 +1,41 @@
+﻿using Base;
+using Data.Commands.ClientContacts.WorkGroup;
+using Data.Entities.Clients;
+using Data.Entities.Users;
+
+namespace Data.Entities.ClientContacts
+{
+    public class WorkGroup : Entity
+    {
+        public int EscortManagerId { get; protected set; }
+        public Manager EscortManager { get; protected set; }
+
+        public int RegionalManagerId { get; protected set; }
+        public Manager RegionalManager { get; protected set; }
+
+        public int ClientId { get; protected set; }
+        public Client Client { get; protected set; }
+
+        protected WorkGroup()
+        {
+
+        }
+
+        public WorkGroup(WorkGroupCreate command)
+        {
+            ClientId = command.ClientId;
+            RegionalManagerId = command.RegionalManagerId;
+            EscortManagerId = command.EscortManagerId;
+        }
+
+        public void ChangeEscortManager(int escortManagerId)
+        {
+            EscortManagerId = escortManagerId;
+        }
+
+        public void ChangeRegionalManager(int regionalManagerId)
+        {
+            RegionalManagerId = regionalManagerId;
+        }
+    }
+}
