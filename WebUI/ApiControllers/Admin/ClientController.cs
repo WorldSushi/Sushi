bc8c1b7f82402f7ca41589a9e9fdde2a2fae91c2
Denis <letto-l@yandex.ru>
2019-07-06 14:41:41 +0300

    Admin API Client

--- WebUI/ApiControllers/Admin/ClientController.cs
+++ WebUI/ApiControllers/Admin/ClientController.cs
@@ -1,10 +1,87 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Data.Commands.Clients;
+using Data.DTO.Clients;
+using Data.Entities.Clients;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebUI.ApiControllers.Admin
 {
-    [Route("api/[controller]")]
+    [Route("api/admin/[controller]")]
     [ApiController]
     public class ClientController : ControllerBase
     {
+        private readonly ApplicationContext _context;
+
+        public ClientController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _context.Set<Client>()
+                .Select(x => new ClientDto()
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    LegalEntity = x.LegalEntity,
+                    Phone = x.Phone,
+                    ClientType = x.ClientType,
+                    NumberOfCalls = x.NumberOfCalls,
+                    NumberOfShipments = x.NumberOfShipments
+                }).ToListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ClientCreate command)
+        {
+            var client = await _context.Set<Client>()
+                .AddAsync(new Client(command));
+
+            await _context.SaveChangesAsync();
+
+            var result = new ClientDto()
+            {
+                Id = client.Entity.Id,
+                Title = client.Entity.Title,
+                LegalEntity = client.Entity.LegalEntity,
+                Phone = client.Entity.Phone,
+                ClientType = client.Entity.ClientType,
+                NumberOfCalls = client.Entity.NumberOfCalls,
+                NumberOfShipments = client.Entity.NumberOfShipments
+            };
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] ClientEdit command)
+        {
+            var client = await _context.Set<Client>()
+                .FirstOrDefaultAsync(x => x.Id == command.Id);
+
+            client.Edit(command);
+
+            await _context.SaveChangesAsync();
+
+            var result = new ClientDto()
+            {
+                Id = client.Id,
+                Title = client.Title,
+                LegalEntity = client.LegalEntity,
+                Phone = client.Phone,
+                ClientType = client.ClientType,
+                NumberOfCalls = client.NumberOfCalls,
+                NumberOfShipments = client.NumberOfShipments
+            };
+
+            return Ok(result);
+        }
     }
 }
