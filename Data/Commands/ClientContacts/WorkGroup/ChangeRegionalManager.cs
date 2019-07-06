e50fc4ef663cbe127259aaa483e5eabc71847574
Denis <letto-l@yandex.ru>
2019-07-06 18:21:34 +0300

    Admin API WorkGroup

---
+++ Data/Commands/ClientContacts/WorkGroup/ChangeRegionalManager.cs
@@ -0,0 +1,8 @@
+﻿namespace Data.Commands.ClientContacts.WorkGroup
+{
+    public class ChangeRegionalManager
+    {
+        public int WorkGroupId { get; set; }
+        public int RegionalManagerId { get; set; }
+    }
+}
---
+++ WebUI/ApiControllers/Admin/WorkGroupController.cs
@@ -0,0 +1,134 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Data.Commands.ClientContacts.WorkGroup;
+using Data.DTO.Clients;
+using Data.Entities.ClientContacts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebUI.ApiControllers.Admin
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    public class WorkGroupController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public WorkGroupController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _context.Set<WorkGroup>()
+                .Select(x => new WorkGroupDto()
+                {
+                    EscortManagerId = x.EscortManagerId,
+                    RegionalManagerId = x.RegionalManagerId
+                }).ToListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] WorkGroupCreate command)
+        {
+            if (await _context.Set<WorkGroup>().AnyAsync(x => x.RegionalManagerId == command.RegionalManagerId
+                                                   || x.EscortManagerId == command.EscortManagerId))
+                return BadRequest("Менеджер уже задействован в другой группе");
+
+            var workGroup = await _context.Set<WorkGroup>()
+                .AddAsync(new WorkGroup(command));
+
+            await _context.SaveChangesAsync();
+
+            var result = new WorkGroupDto()
+            {
+                EscortManagerId = workGroup.Entity.EscortManagerId,
+                RegionalManagerId = workGroup.Entity.RegionalManagerId
+            };
+
+            return Ok(result);
+        }
+
+        [HttpPut("ChangeRegionalManager")]
+        public async Task<IActionResult> Put([FromBody] ChangeRegionalManager command)
+        {
+            if (await _context.Set<WorkGroup>().AnyAsync(x => x.RegionalManagerId == command.RegionalManagerId
+                                                              || x.EscortManagerId == command.RegionalManagerId))
+                return BadRequest("Менеджер уже состоит в другой группе");
+
+            var workGroup = await _context.Set<WorkGroup>()
+                .FirstOrDefaultAsync(x => x.Id == command.WorkGroupId);
+
+            if (workGroup == null)
+                return BadRequest("Рабочая группа не найдена");
+
+            workGroup.ChangeRegionalManager(command.RegionalManagerId);
+
+            await _context.SaveChangesAsync();
+
+            var result = new WorkGroupDto()
+            {
+                EscortManagerId = workGroup.EscortManagerId,
+                RegionalManagerId = workGroup.RegionalManagerId
+            };
+
+            return Ok(result);
+        }
+
+        [HttpPut("ChangeEscortManager")]
+        public async Task<IActionResult> Put([FromBody] ChangeEscortManager command)
+        {
+            if (await _context.Set<WorkGroup>().AnyAsync(x => x.RegionalManagerId == command.EscortManagerId
+                                                              || x.EscortManagerId == command.EscortManagerId))
+                return BadRequest("Менеджер уже состоит в другой группе");
+
+            var workGroup = await _context.Set<WorkGroup>()
+                .FirstOrDefaultAsync(x => x.Id == command.WorkGroupId);
+
+            if (workGroup == null)
+                return BadRequest("Рабочая группа не найдена");
+
+            workGroup.ChangeEscortManager(command.EscortManagerId);
+
+            await _context.SaveChangesAsync();
+
+            var result = new WorkGroupDto()
+            {
+                EscortManagerId = workGroup.EscortManagerId,
+                RegionalManagerId = workGroup.RegionalManagerId
+            };
+
+            return Ok(result);
+        }
+
+        [HttpPut("BindClient")]
+        public async Task<IActionResult> Put([FromBody] BindClient command)
+        {
+            if (await _context.Set<ClientWorkGroup>().AnyAsync(x => x.ClientId == command.ClientId))
+                return BadRequest("Клиет закреплён за другой группой");
+
+            var workGroup = await _context.Set<WorkGroup>()
+                .FirstOrDefaultAsync(x => x.Id == command.WorkGroupId);
+
+            if (workGroup == null)
+                return BadRequest("Группа не найдёна");
+
+            workGroup.BindClient(command);
+
+            await _context.SaveChangesAsync();
+
+            var result = new WorkGroupDto()
+            {
+                EscortManagerId = workGroup.EscortManagerId,
+                RegionalManagerId = workGroup.RegionalManagerId
+            };
+
+            return Ok(result);
+        }
+    }
+}
