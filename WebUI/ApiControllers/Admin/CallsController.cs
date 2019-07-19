b0c3faba7095f159019b70b8f94ba0abff1ba9d8
Denis <letto-l@yandex.ru>
2019-07-19 18:16:01 +0300

    1

---
+++ WebUI/ApiControllers/Admin/CallsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ApiControllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallsController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public CallsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            //var result = await _context.Set<Call>()
+
+            return Ok();
+        }
+    }
+}
---
+++ WebUI/ApiControllers/Admin/WeekPlanController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Data.DTO.Clients;
+using Data.Entities.ClientContacts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebUI.ApiControllers.Admin
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    public class WeekPlanController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public WeekPlanController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _context.Set<WeekPlan>()
+                .Select(x => new WeekPlanDto()
+                {
+                    Id = x.Id,
+                    ClientId = x.ClientId,
+                    Plan = x.Plan,
+                    Fact = x.Fact,
+                    WeekNumber = x.WeekNumber,
+                    ManagerType = x.ManagerType
+                }).ToListAsync();
+
+            return Ok(result);
+        }
+    }
+}
